ed66358ab2db1435cc318ad97740c656e46a335a
frangild <32389531+frangild@users.noreply.github.com>
2020-02-24 17:25:22 +0000

    Auth route fix

--- src/views/auth/Login.js
+++ src/views/auth/Login.js
@@ -34,7 +34,7 @@ export default class Login extends Component {
       password: this.state.password
     }
 
-    axios.post((process.env.REACT_APP_BACKEND||'http://localhost:4000/account/') + 'login', user)
+    axios.post((process.env.REACT_APP_BACKEND||'http://localhost:4000/') + 'account/login', user)
       .then(res => {
         // save token in local storage
         localStorage.setItem('jwtToken', res.data.token);
--- src/views/auth/Register.js
+++ src/views/auth/Register.js
@@ -36,7 +36,7 @@ export default class Register extends Component {
       password: this.state.password
     }
 
-    axios.post((process.env.REACT_APP_BACKEND||'http://localhost:4000/account/') + 'register', user)
+    axios.post((process.env.REACT_APP_BACKEND||'http://localhost:4000/') + 'account/register', user)
       .then(res => console.log(res.data))
       .catch(err => console.log(err));
 
